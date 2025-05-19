6549a72afc6cd01dfeb0e8b0ab701701024594ba
Elahe Goodarzi <el.goodarzi2010@gmail.com>
2025-05-19 20:46:44 +0200

    feat: try to fetch 30days repos and store it on repoList component

--- src/app/pages/repo-list/repo-list.component.ts
+++ src/app/pages/repo-list/repo-list.component.ts
@@ -1,13 +1,48 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+
 import { RepoCardComponent } from '../../components/repo-card/repo-card.component';
+import { SimplifiedRepo, GithubService } from '../../services/github.service';
 
 @Component({
   selector: 'app-repo-list',
   standalone: true,
-  imports: [RepoCardComponent],
+  imports: [CommonModule, RepoCardComponent, InfiniteScrollModule],
   templateUrl: './repo-list.component.html',
   styleUrl: './repo-list.component.scss'
 })
 export class RepoListComponent {
+  public repos: SimplifiedRepo[] = [];
+  private page = 1;
+  public isLoading = false;
+  public error: string | null = null;
 
+  constructor(private githubService: GithubService) {}
+
+  ngOnInit(): void {
+    this.loadRepos();
+  }
+
+  public loadRepos() {
+    if (this.isLoading) return;
+    this.isLoading = true;
+    this.githubService.getTrendingRepos(this.page).subscribe(data => {
+      this.repos = [...this.repos, ...data];
+      this.page++;
+      this.isLoading = false;
+    },
+    error => {
+      if (error instanceof Error) {
+        this.error = error.message;
+      } else {
+        this.error = 'Something went wrong. Please try again.';
+      }
+      this.isLoading = false;
+    });
+  }
+
+  public retry() {
+    this.loadRepos();
+  }
 }
--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,9 +1,59 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Observable, catchError, throwError } from 'rxjs';
+
+export interface SimplifiedRepo {
+  name: string;
+  description: string;
+  stars: string;
+  issues: number;
+  ownerLogin: string;
+  avatarUrl: string;
+  pushedAt: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
+  private BASE_URL = 'https://api.github.com/search/repositories';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public getTrendingRepos(page: number, perPage: number = 10): Observable<SimplifiedRepo[]> {
+    const date = this.getDate30DaysAgo();
+    const url = `${this.BASE_URL}?q=created:>${date}&sortt=stars&order=desc&per_page=${perPage}&page=${page}`;
+
+    return this.http.get<any>(url).pipe(
+      map(res =>
+        res.items.map((repo: any) => ({
+          name: repo.name,
+          description: repo.description,
+          stars: this.formatStars(repo.stargazers_count),
+          issues: repo.open_issues_count,
+          ownerLogin: repo.owner.login,
+          avatarUrl: repo.owner.avatar_url,
+          pushedAt: repo.pushed_at
+        }),
+        catchError((error) => {
+          console.error('GitHub API error:', error);
+          return throwError(() => new Error('Failed to load. Please try again later.'));
+        })
+      ))
+    );
+  }
+
+  private getDate30DaysAgo(): string {
+    const date = new Date();
+    date.setDate(date.getDate() - 30);
+    return date.toISOString().split('T')[0];
+  }
+
+  private formatStars(count: number): string {
+    if (count >= 1000) {
+      return `${(count / 1000).toFixed(1).replace('.0', '')}k`;
+    }
+    return count.toString();
+  }
 }
